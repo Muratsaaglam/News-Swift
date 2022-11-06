b3b00bbaf61819f9d9ae1c6bed830f5bfa696307
Muratsaaglam <muratsaaglam@outlook.com>
2022-11-06 13:52:58 +0300

    Initial Commit

---
+++ News/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  News
+//
+//  Created by Murat Sağlam on 6.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
